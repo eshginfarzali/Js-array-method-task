b67b77142d0b4f7978e81071513f9729f6ea3b36
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-01-19 19:58:38 +0400

    task1

---
+++ app.js
@@ -0,0 +1,19 @@
+/*//Task-clone array with array methods . min: 2 case
+
+
+//#case1
+// Cloning array using spread
+
+const oldArray= ["1", "2", "3"];
+
+const clonedArray= ["0", ...oldArray, "4"];
+
+//#case2
+// Cloning array using slice
+console.log(clonedArray);
+
+let array = ['JavaScript', 'Copy', 'Array', 'Example'];
+
+console.log(array.slice());
+
+*/
